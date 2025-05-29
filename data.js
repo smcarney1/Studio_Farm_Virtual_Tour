0d5e4e7de32f0a83b1eb378bd6dc9840a04d7a72
smcarney1 <smcarney1@gmail.com>
2025-05-29 15:02:56 -0600

    Upload

---
+++ data.js
@@ -0,0 +1,389 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-buildings",
+      "name": "Buildings",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.30229967075559117,
+        "pitch": 0.5817086675548513,
+        "fov": 1.3879620877225907
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4341527112014756,
+          "pitch": -0.035870733123392284,
+          "rotation": 21.991148575128566,
+          "target": "1-trees"
+        },
+        {
+          "yaw": 0.25321222384884656,
+          "pitch": -0.03287811166306298,
+          "rotation": 3.141592653589793,
+          "target": "2-northwest-of-trees"
+        },
+        {
+          "yaw": -0.21010067672990473,
+          "pitch": -0.022234889550738046,
+          "rotation": 3.141592653589793,
+          "target": "5-west-side"
+        },
+        {
+          "yaw": 0.7260205458209317,
+          "pitch": 0.08741519179236512,
+          "rotation": 6.283185307179586,
+          "target": "3-middle"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-trees",
+      "name": "Trees",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.39686539319961156,
+        "pitch": 0.5752916818717893,
+        "fov": 1.3879620877225907
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4071012457563263,
+          "pitch": -0.04548310100525299,
+          "rotation": 9.42477796076938,
+          "target": "0-buildings"
+        },
+        {
+          "yaw": 0.19617888848756593,
+          "pitch": 0.021776339344976137,
+          "rotation": 3.141592653589793,
+          "target": "2-northwest-of-trees"
+        },
+        {
+          "yaw": 2.706087545146005,
+          "pitch": 0.26021766651774314,
+          "rotation": 0,
+          "target": "3-middle"
+        },
+        {
+          "yaw": -0.6561903447537638,
+          "pitch": 0.021323257649596528,
+          "rotation": 3.141592653589793,
+          "target": "5-west-side"
+        },
+        {
+          "yaw": 1.3512415228012813,
+          "pitch": 0.10269174903118383,
+          "rotation": 0,
+          "target": "4-north-middle"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-northwest-of-trees",
+      "name": "Northwest of Trees",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.8076830853788834,
+        "pitch": 0.592396761631985,
+        "fov": 1.3879620877225907
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0288300139937192,
+          "pitch": 0.07516881989220181,
+          "rotation": 0,
+          "target": "1-trees"
+        },
+        {
+          "yaw": -0.5073179738375728,
+          "pitch": -0.034439300272786966,
+          "rotation": 3.141592653589793,
+          "target": "0-buildings"
+        },
+        {
+          "yaw": 0.6251947508695928,
+          "pitch": 0.034469083525532795,
+          "rotation": 3.141592653589793,
+          "target": "5-west-side"
+        },
+        {
+          "yaw": -2.135976330540771,
+          "pitch": 0.10835580428414104,
+          "rotation": 0,
+          "target": "4-north-middle"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-middle",
+      "name": "Middle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.5627583093166741,
+        "pitch": 0.5474858158042935,
+        "fov": 1.3879620877225907
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24997958457549885,
+          "pitch": -0.02339079960156809,
+          "rotation": 3.141592653589793,
+          "target": "1-trees"
+        },
+        {
+          "yaw": 0.5634378621911367,
+          "pitch": -0.024145349326104792,
+          "rotation": 3.141592653589793,
+          "target": "2-northwest-of-trees"
+        },
+        {
+          "yaw": -1.63778649138232,
+          "pitch": -0.046254628386666496,
+          "rotation": 3.141592653589793,
+          "target": "0-buildings"
+        },
+        {
+          "yaw": -0.1512056549773657,
+          "pitch": -0.01799692289697319,
+          "rotation": 3.141592653589793,
+          "target": "5-west-side"
+        },
+        {
+          "yaw": 1.362734355290442,
+          "pitch": 0.12512934953308452,
+          "rotation": 0,
+          "target": "4-north-middle"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-north-middle",
+      "name": "North Middle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.625659252637238,
+        "pitch": 0.5278133240171634,
+        "fov": 1.3879620877225907
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.419931268614544,
+          "pitch": 0.014332610218671604,
+          "rotation": 3.141592653589793,
+          "target": "1-trees"
+        },
+        {
+          "yaw": 0.033858763947602455,
+          "pitch": 0.11124984889145217,
+          "rotation": 0,
+          "target": "3-middle"
+        },
+        {
+          "yaw": 0.07066560425083779,
+          "pitch": -0.009070270265551628,
+          "rotation": 3.141592653589793,
+          "target": "0-buildings"
+        },
+        {
+          "yaw": 1.1948646828725202,
+          "pitch": 0.0012121632306243413,
+          "rotation": 3.141592653589793,
+          "target": "2-northwest-of-trees"
+        },
+        {
+          "yaw": 0.9749155370612428,
+          "pitch": -0.0010465965893970974,
+          "rotation": 3.141592653589793,
+          "target": "5-west-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-west-side",
+      "name": "West Side",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.024804087074981496,
+        "pitch": 0.5367822437045326,
+        "fov": 1.3879620877225907
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7216399425395821,
+          "pitch": -0.012281811598606751,
+          "rotation": 3.141592653589793,
+          "target": "0-buildings"
+        },
+        {
+          "yaw": -0.0805933068550857,
+          "pitch": 0.023141342115881258,
+          "rotation": 3.141592653589793,
+          "target": "1-trees"
+        },
+        {
+          "yaw": -0.917910674484153,
+          "pitch": 0.1199188777263398,
+          "rotation": 0,
+          "target": "2-northwest-of-trees"
+        },
+        {
+          "yaw": -0.6663129652096433,
+          "pitch": 0.04073489976161149,
+          "rotation": 0,
+          "target": "4-north-middle"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Studio Farm",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
